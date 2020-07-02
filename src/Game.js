09b246b61cf9594d60d82b7ca8b83e94a1b40183
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-07-02 17:45:04 -0600

    Create createcards method

--- src/Game.js
+++ src/Game.js
@@ -3,7 +3,7 @@ const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 const Round = require('./Round');
 const Deck = require('./Deck');
-// const Card = require('./Card');
+const Card = require('./Card');
 
 class Game {
   constructor() {
@@ -11,14 +11,20 @@ class Game {
   }
 
   start() {
-    const cards = prototypeQuestions;
+    const cards = this.createCards(prototypeQuestions);
     const deck = new Deck(cards);
     this.currentRound = new Round(deck);
     this.printMessage(deck, this.currentRound);
     this.printQuestion(this.currentRound);
   }
 
-  printMessage(deck, round) {
+  createCards(cardSource) {
+    return cardSource.map((card) => {
+      return new Card(card.id, card.question, card.answers, card.correctAnswer);
+    });
+  }
+
+  printMessage(deck) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
